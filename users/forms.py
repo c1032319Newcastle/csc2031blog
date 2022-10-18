5a191945d2f9bbe833cf03129e6b216bb0ec260d
sakainewman <c1032319@newcastle.ac.uk>
2022-10-18 16:12:09 +0100

    Attempted to add regex validator

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+import re
+
+
+def validate_data(self, data_field):
+    p = re.compile("/d + [a-z]/g")
+    if not p.match(data_field.data):
+        raise ValidationError("MIn. 1 digit and 1 lower case letter required")
+
+
+def char_check(form, field):
+    excluded_char = "&<>%"
+
+    for char in field.data:
+        if char in excluded_char:
+            raise ValidationError(f"Char {char} is not allowed")
 
 
 class RegisterForm(FlaskForm):
-    username = EmailField(validators=[DataRequired(), Email()])
-    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15)])
+    username = EmailField(validators=[DataRequired(), Email(), char_check])
+    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15), char_check])
     submit = SubmitField()
