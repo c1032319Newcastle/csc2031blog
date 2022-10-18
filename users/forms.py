ef16d9ceba1d193f98f4e137b502e6af262bb51f
sakainewman <c1032319@newcastle.ac.uk>
2022-10-18 15:12:57 +0100

    Added email validator and package in requirements

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Email
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[DataRequired()])
+    username = EmailField(validators=[DataRequired(), Email()])
     password = PasswordField(validators=[DataRequired()])
     submit = SubmitField()
