7c57fa17aa35b6bad41771e895ed32bd2ae60ee9
sakainewman <c1032319@newcastle.ac.uk>
2022-10-25 14:55:41 +0100

    Updated error handlers

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 
 @app.errorhandler(403)
-def internal_error(error):
+def forbidden_error(error):
     return render_template('403.html'), 403
 
+
 @app.errorhandler(404)
-def internal_error(error):
+def not_found_error(error):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
