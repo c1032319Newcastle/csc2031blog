bb9e2270e477d7793b905f44194675972d0bfcee
sakainewman <c1032319@newcastle.ac.uk>
2022-10-25 16:32:53 +0100

    Initialised database

--- models.py
+++ models.py
@@ -1,13 +1,19 @@
-from app import db
+from app import db, app
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key='True')
-    username = db.Column(db.String(100), nullable='False', unique='True')
-    password = db.Column(db.String(100), nullable='False')
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+def init_db():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
